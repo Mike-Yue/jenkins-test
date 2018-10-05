6129418846c80cc6c7fb6f37d03681eb2f3881ab
Mike Yue <myue@bccrc.ca>
2018-10-05 14:55:27 -0700

    branch

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import datetime
+from dateutil import tz
 
 if __name__ == "__main__":
 	print(datetime.date.today())
-	print(datetime.datetime.now())
+	utc = datetime.datetime.now()
+	my_zone = tz.tzlocal()
+	real_time = utc.replace(tzinfo=my_zone)
+	print real_time
