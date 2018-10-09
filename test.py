0a413dc94dc94ea33707d1af13f2c95222497f20
Mike Yue <myue@bccrc.ca>
2018-10-09 09:25:31 -0700

    regression

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from dateutil import tz
 if __name__ == "__main__":
 	print(datetime.date.today())
 	utc = datetime.datetime.now()
-	my_zone = tz.tzlocal()
-	real_time = utc.replace(tzinfo=my_zone)
-	print real_time
+	print utc
+	#my_zone = tz.tzlocal()
+	#real_time = utc.replace(tzinfo=my_zone)
+	#print real_time
