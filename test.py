30497b0c713305de5c003813e8113cff2f4b9b08
Mike Yue <myue@bccrc.ca>
2018-10-09 09:29:53 -0700

    idk

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
 	print(datetime.date.today())
 	utc = datetime.datetime.now()
 	print utc
+	print ("lmao idk what's going on")
 	#my_zone = tz.tzlocal()
 	#real_time = utc.replace(tzinfo=my_zone)
 	#print real_time
