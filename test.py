f573b539f854fa3182bba63e50845c45cfec43ec
Mike Yue <myue@bccrc.ca>
2018-10-09 09:31:21 -0700

    what is going on

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import datetime
 from dateutil import tz
 
 if __name__ == "__main__":
-	print(datetime.date.today())
+	#print(datetime.date.today())
 	utc = datetime.datetime.now()
 	print utc
 	print ("lmao idk what's going on")
+
 	#my_zone = tz.tzlocal()
 	#real_time = utc.replace(tzinfo=my_zone)
 	#print real_time
